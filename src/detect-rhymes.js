e23ed6373ab3b8568789d9e28dee9221b54f8756
Lee Meyer <lee.meyer@gmail.com>
2020-10-06 21:34:38 +1100

    first commit

---
+++ src/detect-rhymes.js
@@ -0,0 +1,77 @@
+var cmuDictionary = require('cmu-pronouncing-dictionary');
+
+module.exports = function(message) {
+
+    const noiseWords = ['the', 'a', 'an'];
+    let rhymeGroups = {};
+
+    let words = message.split(/\b/); 
+
+    for (let word of words)
+    {
+        if (word.length > 1 && noiseWords.indexOf(word.toLowerCase()) === -1)
+        {
+            let phonemes = cmuDictionary[word.toLowerCase()];
+
+            if (phonemes) {
+                let santizedPhonemes = phonemes.replace(/\d/g,'').replace(/UW R$/, 'AO').replace(/UH R$/, 'AO').replace(/AO R$/, 'AO').replace(/EH L/, 'AH L').replace(/AA/g, 'AO');
+
+                let heuristics = [/AY T$/, /EH D$/]
+
+                for (let heuristic of heuristics)
+                {                
+                    if (santizedPhonemes.match(heuristic)) {
+                        santizedPhonemes = heuristic.toString().replace(/[\/\$]/g, "");
+                    }
+                }
+                            
+                let phonemesArray = santizedPhonemes.split(' ');
+
+                let lastVowelIndex = 0, secondLastVowelIndex, vowelsCount = 0;
+        
+                for (let j = 0; j < phonemesArray.length; j++)
+                {
+                    if (phonemesArray[j].match(/^[AEIOU]/i))
+                    {
+                        secondLastVowelIndex = lastVowelIndex;
+                        lastVowelIndex = j;
+                        vowelsCount++;
+                    }
+                }
+        
+                let endPhonemesArray = phonemesArray.slice(vowelsCount > 1 && secondLastVowelIndex > 0 ? secondLastVowelIndex : lastVowelIndex);
+                let endPhoneme = endPhonemesArray.join(' ');
+            
+                for (let key in rhymeGroups)
+                {
+                    if (endPhoneme.endsWith(key) || key.endsWith(endPhoneme)) 
+                        endPhoneme = key;
+                }
+
+                if (!rhymeGroups[endPhoneme])
+                {
+                    rhymeGroups[endPhoneme] = [];
+                }
+        
+                if (rhymeGroups[endPhoneme].indexOf(word.toLowerCase()) < 0)
+                {
+                    rhymeGroups[endPhoneme].push(word.toLowerCase());
+                }
+            }
+        }
+    }
+
+    let returnArray = [];
+    
+    for (let key in rhymeGroups) {
+        if (rhymeGroups[key].length < 2) {
+            delete rhymeGroups[key];
+        }
+        else 
+        {
+            returnArray.push(rhymeGroups[key]);
+        }
+    }
+
+    return returnArray;
+}
---
+++ tests/detect-rhymes.test.js
@@ -0,0 +1,60 @@
+const detectRyhmes = require("../src/detect-rhymes");
+const fs = require("fs");
+const path = require("path");
+
+test('Detects rhymes in the man from snowy river', () => {
+    let content = fs.readFileSync(path.resolve(__dirname, "the-man-from-snowy-river.txt")).toString();
+    let rhymes = detectRyhmes(content);
+    expect(rhymes).toMatchSnapshot();
+});
+
+test('Detects rhymes in untitled drowning poem', () => {
+    let content = fs.readFileSync(path.resolve(__dirname, "untitled.txt")).toString();
+    let rhymes = detectRyhmes(content);
+    expect(rhymes).toMatchSnapshot();
+});
+
+
+test('Every end rhyme is detected in the man from snowy river', () => {
+    let content = fs.readFileSync(path.resolve(__dirname, "the-man-from-snowy-river.txt")).toString();
+    let endWords = content.split('\r\n').filter(l => l.match(/(\w+)/g)).map(l => l.match(/(\w+)/g).pop()); 
+   
+    let rhymes = detectRyhmes(content);
+    
+    for (let i = 0; i < endWords.length; i+=4)
+    {
+        let rhymeGroup1 = rhymes.find(r => r.indexOf(endWords[i]) > -1);
+        expect(rhymeGroup1).not.toBeNull(); 
+        expect(rhymeGroup1).toContain(endWords[i + 2]);
+
+        let rhymeGroup2 = rhymes.find(r => r.indexOf(endWords[i + 1]) > -1);
+        expect(rhymeGroup2).not.toBeNull(); 
+        expect(rhymeGroup2).toContain(endWords[i + 3]);
+    }
+})
+
+test("should detect long and short rhyming words", () => {
+    let rhymes = detectRyhmes("overnight delight intertwine emboss mine loss shine boss refine incline");
+    expect(rhymes.length).toBe(3);
+
+    expect(rhymes[0]).toEqual(expect.arrayContaining([ 'overnight', 'delight' ]));
+    expect(rhymes[0].length).toBe(2);
+
+    expect(rhymes[1]).toEqual(expect.arrayContaining([ 'intertwine', 'mine', 'shine', 'incline' ]));
+    expect(rhymes[1].length).toBe(4);
+
+    expect(rhymes[2]).toEqual(expect.arrayContaining(['emboss', 'loss', 'boss' ]));
+    expect(rhymes[2].length).toBe(3);
+})
+
+test('man from snowy river rhyme detection should run faster than 100ms', async () => {
+    let content = fs.readFileSync(path.resolve(__dirname, "the-man-from-snowy-river.txt")).toString();
+
+    let t0 = performance.now();
+
+    detectRyhmes(content);
+
+    var t1 = performance.now();
+
+    expect(t1 - t0).toBeLessThan(100);
+});
